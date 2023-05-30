428b77f89003fb5bad9898b6e052e58efa821b38
yehudaheller <116564740+yehudaheller@users.noreply.github.com>
2023-05-30 19:34:39 +0300

    add asyncio.gather to allow multitasking(requests)

--- openai_integration.py
+++ openai_integration.py
@@ -4,10 +4,8 @@ import asyncio
 import openai
 import os
 
-
 # Load the environment variables from the .env file
 load_dotenv()
-
 
 # Counter for the number of requests made
 request_counter = 0
@@ -35,6 +33,34 @@ def set_api_key(api_key: str) -> None:
     openai.api_key = api_key
 
 
+async def make_openai_request(prompt: str) -> str:
+    """
+    Makes a single request to the OpenAI API.
+
+    Args:
+        prompt (str): The prompt to send to the API.
+
+    Returns:
+        str: The generated response.
+    """
+    global request_counter
+    global last_request_time
+
+    response = await asyncio.to_thread(
+        openai.ChatCompletion.create,
+        model="gpt-3.5-turbo",
+        messages=[
+            {"role": "system", "content": prompt},
+            {"role": "user", "content": "Please Explain This"},
+        ],
+    )
+
+    request_counter += 1
+    last_request_time = datetime.now()
+
+    return response.choices[0].message.content.strip()
+
+
 async def integrate_openai(slides_text: list[str]) -> str:
     """
     Integrates with the OpenAI API to generate summaries for each slide text.
@@ -49,37 +75,30 @@ async def integrate_openai(slides_text: list[str]) -> str:
     global last_request_time
 
     openai.api_key = os.environ.get("API_KEY")
-    model_engine = "gpt-3.5-turbo"
     final_generated_text = ""
 
+    tasks = []
     for i, slide_text in enumerate(slides_text):
         prompt = "Write an explanation of the following:\n"
         prompt += slide_text + "\n"
 
-        response = await asyncio.to_thread(
-            openai.ChatCompletion.create,
-            model=model_engine,
-            messages=[
-                {"role": "system", "content": prompt},
-                {"role": "user", "content": "Please Explain This"},
-            ],
-        )
+        task = asyncio.create_task(make_openai_request(prompt))
+        tasks.append(task)
 
-        request_counter += 1
-        last_request_time = datetime.now()
+        if len(tasks) == requests_per_interval or i == len(slides_text) - 1:
+            await asyncio.gather(*tasks)
 
-        generated_text = response.choices[0].message.content.strip()
-        final_generated_text += generated_text
+            for task in tasks:
+                generated_text = await task
+                final_generated_text += generated_text + "\n"
+            tasks.clear()
 
-        if (
-            i < len(slides_text) - 1
-            and request_counter % requests_per_interval == 0
-        ):
-            time_diff = datetime.now() - last_request_time
-            remaining_time = timedelta(minutes=interval_minutes) - time_diff
+            if i < len(slides_text) - 1:
+                time_diff = datetime.now() - last_request_time
+                remaining_time = timedelta(minutes=interval_minutes) - time_diff
 
-            if remaining_time.total_seconds() > 0:
-                print("Waiting for 60 seconds before the next request...")
-                await asyncio.sleep(remaining_time.total_seconds())
+                if remaining_time.total_seconds() > 0:
+                    print("Waiting for 60 seconds before the next request...")
+                    await asyncio.sleep(remaining_time.total_seconds())
 
     return final_generated_text
