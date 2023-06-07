381307ca4e3f0ada25bb0e994dab357f4e39efc8
yehudaheller <116564740+yehudaheller@users.noreply.github.com>
2023-06-07 21:13:37 +0300

    save the data to json file

--- json_utils.py
+++ json_utils.py
@@ -1,16 +1,20 @@
 import json
+import os
+import re
 
 
-def save_to_json(generate_text: str, file_name: str) -> None:
+def save_to_json(generated_text, output_file_name):
     """
-    Saves the generated text to a JSON file.
-
-    Args:
-        generate_text (str): The generated text to be saved.
-        file_name (str): The name of the JSON file to save.
-
-    Returns:
-        None
+    Save the generated text to a JSON file.
     """
-    with open(file_name, "w") as f:
-        json.dump(generate_text, f)
+    output_folder = os.path.abspath('outputs')  # Specify the absolute path to the output folder
+    os.makedirs(output_folder, exist_ok=True)  # Create the output folder if it doesn't exist
+    print(output_file_name)
+    print(output_folder)
+    #output_file_path = os.path.join(output_folder, output_file_name)
+
+    new_file_path = re.sub(r"uploads\\", r"outputs\\", output_file_name)
+
+    with open(new_file_path, "w") as f:
+        json.dump(generated_text, f)
+
--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 import asyncio
+import os
+
 from os import path
 from pptx_parser import read_pptx_file, get_pptx_file_path
 from openai_integration import integrate_openai
@@ -9,23 +11,30 @@ async def main() -> None:
     """
     Main function to orchestrate the PowerPoint file processing and OpenAI integration.
     """
-    # Get the path to the PowerPoint file
-    pptx_file_path = get_pptx_file_path()
 
-    # Call the read_pptx_file function to read the PowerPoint file
-    slides_text = read_pptx_file(pptx_file_path)
-    print(slides_text)
+    while True:
+        asyncio.sleep(10)
+        # Scan the uploads folder
+        files = os.listdir('uploads')
 
-    # Integrate OpenAI asynchronously
-    generate_text = await integrate_openai(slides_text)
-    print(generate_text)
+        # Process each file
+        for file in files:
+            pptx_file_path = os.path.join('uploads', file)
 
-    # Get the output file name based on the PowerPoint file name
-    file_name_without_extension = path.splitext(pptx_file_path)[0]
-    output_file_name = file_name_without_extension + ".json"
+            # Call the read_pptx_file function to read the PowerPoint file
+            slides_text = read_pptx_file(pptx_file_path)
+            print(slides_text)
 
-    # Save the generated text to a JSON file with the same name as the original presentation
-    save_to_json(generate_text, output_file_name)
+            # Integrate OpenAI asynchronously
+            generate_text = await integrate_openai(slides_text)
+            print(generate_text)
+
+            # Get the output file name based on the PowerPoint file name
+            file_name_without_extension = path.splitext(pptx_file_path)[0]
+            output_file_name = file_name_without_extension + ".json"
+
+            # Save the generated text to a JSON file with the same name as the original presentation
+            save_to_json(generate_text, output_file_name)
 
 
 if __name__ == "__main__":
